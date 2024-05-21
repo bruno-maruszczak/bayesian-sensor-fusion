9b86eccd8222586807a5429c5c8e16eff3fcf785
bruno-maruszczak <72678261+bruno-maruszczak@users.noreply.github.com>
2024-05-21 20:23:51 +0200

    Building a factor graph from odometry messages with placeholder variance

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -30,7 +30,7 @@
 #include <gtsam/nonlinear/Values.h>
 using std::placeholders::_1;
 using namespace std::chrono_literals;
-
+using namespace gtsam;
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
 
@@ -38,13 +38,13 @@ class Minimal : public rclcpp::Node
 {
 public:
   Minimal()
-  : Node("minimal_publisher"), count_(0)
+  : Node("minimal_publisher"), count_(0), is_graph_initialised_(false), graph_n_(0)
   {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
       "odom", 10, std::bind(&Minimal::topic_callback, this, _1));
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&Minimal::timer_callback, this));
+      1500ms, std::bind(&Minimal::timer_callback, this));
   }
 
 private:
@@ -53,16 +53,34 @@ private:
     auto message = std_msgs::msg::String();
     message.data = "Hello, world! " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    graph_.print("Graph: ");
     publisher_->publish(message);
   }
-  void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg) const
+  void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
   {
     RCLCPP_INFO(this->get_logger(), "I heard something'");
+    auto x = msg->pose.pose.position.x;
+    auto y = msg->pose.pose.position.y;
+    odom_reading_ = Pose2(x, y, 0.0);
+    auto noise = noiseModel::Diagonal::Sigmas(Vector3(0.1,0.1,0.1));
+    if(!is_graph_initialised_){
+    	is_graph_initialised_ = true;
+	graph_n_++;
+	graph_.addPrior(1, odom_reading_, noise);
+    } else {
+   	graph_.emplace_shared<BetweenFactor<Pose2> >(graph_n_, graph_n_ + 1, odom_reading_, noise);
+	graph_n_++;
+    }
   }
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
+  Pose2 odom_reading_;
+  Values graph_values_;
+  NonlinearFactorGraph graph_;
+  bool is_graph_initialised_;
+  size_t graph_n_; // Graph node count
 };
 
 
