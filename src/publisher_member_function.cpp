ccd37ab7f6d654e6e757a4493f2c6072bb00d96e
SpicyHolo <41269364+SpicyHolo@users.noreply.github.com>
2024-05-28 19:07:26 +0200

    Add estimating position using graph, add debug message, refactor code

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -35,57 +35,101 @@ using namespace std::chrono_literals;
 using namespace gtsam;
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
+gtsam::Pose2 operator-(const gtsam::Pose2& pose1, const gtsam::Pose2& pose2) {
+    // Subtract the translation and rotation components separately
+    double dx = pose1.x() - pose2.x();
+    double dy = pose1.y() - pose2.y();
+    double dtheta = pose1.theta() - pose2.theta();
+    // Return the difference as a new Pose2 object
+    return gtsam::Pose2(dx, dy, dtheta);
+}
 
 class Minimal : public rclcpp::Node
 {
 public:
   Minimal()
-  : Node("minimal_publisher"), count_(0), is_graph_initialised_(false), graph_n_(0)
+  : Node("minimal_publisher"), graph_n_(0), debug_(true), last_odom_reading_(Pose2(0.0, 0.0, 0.0))
   {
+    // Add prior knowledge
+    addPrior(Pose2(0.0, 0.0, 0.0), noiseModel::Diagonal::Sigmas(Vector3(0.1,0.1,0.1)));
+
+    // Start publishing, subscribtion and timer
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
       "odom", 10, std::bind(&Minimal::topic_callback, this, _1));
-    timer_ = this->create_wall_timer(
-      1500ms, std::bind(&Minimal::timer_callback, this));
+    /*timer_ = this->create_wall_timer(
+      3000ms, std::bind(&Minimal::timer_callback, this));
+      */
   }
 
 private:
-  void timer_callback()
+  // Given odometry message returns robot position as Pos2(x, y, theta)
+  Pose2 getPoseFromOdom(const nav_msgs::msg::Odometry::SharedPtr msg)
   {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    graph_.print("Graph: ");
-    publisher_->publish(message);
-  }
-  void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
-  {
-    RCLCPP_INFO(this->get_logger(), "I heard something'");
     auto x = msg->pose.pose.position.x;
     auto y = msg->pose.pose.position.y;
     auto theta = quaternionToYaw(msg->pose.pose.orientation);
 
-    odom_reading_ = Pose2(x, y, theta);
-    auto noise = noiseModel::Diagonal::Sigmas(Vector3(0.1,0.1,0.1));
-    if(!is_graph_initialised_){
-    	is_graph_initialised_ = true;
-	graph_n_++;
-	graph_.addPrior(1, odom_reading_, noise);
-    } else {
-   	graph_.emplace_shared<BetweenFactor<Pose2> >(graph_n_, graph_n_ + 1, odom_reading_, noise);
-	graph_n_++;
-    }
+    return Pose2(x, y, theta);
   }
+  
+  void addPrior(Pose2 mean, noiseModel::Diagonal::shared_ptr noise)
+  { 
+    graph_.addPrior(1,  mean, noise);
+    initial_guess_.insert(1, Pose2(0.0, 0.0, 0.0));
+    graph_n_++;
+
+    if (debug_)
+      RCLCPP_INFO(this->get_logger(), "Graph: Set prior to ");
+  }
+
+  void timer_callback()
+  {
+    if (this->debug_)
+      RCLCPP_INFO(this->get_logger(), "Graph: Starting optimizer");
+
+    // Optimise graph
+    LevenbergMarquardtOptimizer optimizer(graph_, initial_guess_);
+    
+    // Estimate positions
+    Values positions = optimizer.optimize();
+    if (this->debug_)
+      RCLCPP_INFO(this->get_logger(), "Graph: Finished optimisation");
+    positions.print("Positions: ");
+    auto message = std_msgs::msg::String();
+    message.data = "Optimised " + std::to_string(graph_n_);
+
+    publisher_->publish(message);
+  }
+
+  void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+  {
+    if (this->debug_)
+    {
+      std::string debug_msg = "Received odometry. " + std::to_string(graph_n_ + 1);
+      RCLCPP_INFO(this->get_logger(), debug_msg.c_str());
+    }
+    odom_reading_ = getPoseFromOdom(msg); 
+    
+    initial_guess_.insert(graph_n_+1, Pose2(0.0, 0.0, 0.0));
+    auto noise = noiseModel::Diagonal::Sigmas(Vector3(0.1,0.1,0.1));
+
+   	graph_.emplace_shared<BetweenFactor<Pose2> >(graph_n_, graph_n_ + 1, odom_reading_ - last_odom_reading_, noise);
+    graph_n_++;
+    this->timer_callback();
+    last_odom_reading_ = odom_reading_;
+  }
+
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+  
   Pose2 odom_reading_;
-  Values graph_values_;
+  Pose2 last_odom_reading_;
+  Values initial_guess_;
   NonlinearFactorGraph graph_;
-  bool is_graph_initialised_;
   size_t graph_n_; // Graph node count
+  bool debug_;
 };
 
 
